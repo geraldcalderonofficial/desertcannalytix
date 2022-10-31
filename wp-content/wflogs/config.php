f9916710b2ad70d7c660d6f6d9550a6cf3198432
Gerald O. Calderon <geraldcalderonofficial@gmail.com>
2022-11-01 06:56:45 +0800

    Add files

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1623099947;s:7:"authKey";s:64:"r*)W4]yV*o;!v&uSrmJc9?@Qk>pqf^XT[n[bf>9YAPG1YJfV;,R[~Z01#[;p`3SS";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:150;}
